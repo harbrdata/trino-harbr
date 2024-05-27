ebyhr/trino

Add support for writing deletion vectors in Delta Lake

plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakePageSinkProvider.java
@@ -51,6 +51,7 @@ import static io.trino.plugin.deltalake.DeltaLakeColumnType.REGULAR;
 import static io.trino.plugin.deltalake.DeltaLakeParquetSchemas.createParquetSchemaMapping;
 import static io.trino.plugin.deltalake.transactionlog.DeltaLakeSchemaSupport.changeDataFeedEnabled;
 import static io.trino.plugin.deltalake.transactionlog.DeltaLakeSchemaSupport.extractSchema;
+import static io.trino.plugin.deltalake.transactionlog.DeltaLakeSchemaSupport.isDeletionVectorEnabled;
 import static io.trino.spi.type.VarcharType.VARCHAR;
 import static java.util.Objects.requireNonNull;
 
@@ -185,6 +186,7 @@ public class DeltaLakePageSinkProvider
                 domainCompactionThreshold,
                 () -> createCdfPageSink(merge, session),
                 changeDataFeedEnabled(tableHandle.metadataEntry(), tableHandle.protocolEntry()).orElse(false),
+                isDeletionVectorEnabled(tableHandle.getMetadataEntry(), tableHandle.getProtocolEntry()),
                 parquetSchemaMapping);
     }
 

harbrdata/trino-harbr

plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakePageSinkProvider.java
@@ -51,6 +51,7 @@ import static io.trino.plugin.deltalake.DeltaLakeColumnType.REGULAR;
 import static io.trino.plugin.deltalake.DeltaLakeParquetSchemas.createParquetSchemaMapping;
 import static io.trino.plugin.deltalake.transactionlog.DeltaLakeSchemaSupport.changeDataFeedEnabled;
 import static io.trino.plugin.deltalake.transactionlog.DeltaLakeSchemaSupport.extractSchema;
+import static io.trino.plugin.deltalake.transactionlog.DeltaLakeSchemaSupport.isDeletionVectorEnabled;
 import static io.trino.spi.type.VarcharType.VARCHAR;
 import static java.util.Objects.requireNonNull;
 
@@ -185,7 +186,9 @@ public class DeltaLakePageSinkProvider
                 domainCompactionThreshold,
                 () -> createCdfPageSink(merge, session),
                 changeDataFeedEnabled(tableHandle.metadataEntry(), tableHandle.protocolEntry()).orElse(false),
-                parquetSchemaMapping);
+                parquetSchemaMapping,
+                isDeletionVectorEnabled(tableHandle.metadataEntry(), tableHandle.protocolEntry()),
+                merge.deletionVectors());
     }
 
     private DeltaLakeCdfPageSink createCdfPageSink(